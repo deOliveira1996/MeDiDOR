b48df3918be8bb5c1eada2bb96ba35dc32699af2
deOliveira1996 <llimaoliveira@hotmail.com>
2026-06-30 10:26:07 -0300

    test_scripts

--- tests/testthat/test-app-Calib.R
+++ tests/testthat/test-app-Calib.R
@@ -3,74 +3,52 @@ library(shinytest2)
 test_that("{shinytest2} recording: calib_gui_test", {
   local_app_support(test_path("../../inst/shiny-apps/Calib"))
   app <- AppDriver$new(test_path("../../inst/shiny-apps/Calib"), name = "calib_gui_test",
-      seed = 123, height = 911, width = 1619)
-  app$click("path")
-  app$click("confirmBtn")
-  calib_wd <- system.file("extdata", "calib_test", package = "MedidoR")
+                       seed = 123, height = 911, width = 1619)
+
+  # 1. Diretório
   app$click("path")
   app$wait_for_idle()
+  calib_wd <- system.file("extdata", "calib_test", package = "MedidoR")
   app$set_inputs(wd = calib_wd)
   app$click("confirmBtn")
   app$wait_for_idle()
+
+  # 2. Inicialização e Imagem
   app$click("import")
   img_calib_path <- system.file("extdata", "calib_test", "Molde_15M_1.png", package = "MedidoR")
   if (img_calib_path == "") stop("Calibration test image not found in the extdata folder.")
   app$upload_file(file = img_calib_path)
-  app$set_inputs(plot_click = character(0), allow_no_input_binding_ = TRUE)
-  app$set_inputs(plot_brush = character(0), allow_no_input_binding_ = TRUE)
+  app$wait_for_idle(timeout = 5000)
+
+  # 3. Metadados
   app$set_inputs(objL = "2")
   app$set_inputs(ImageID = "15m_1")
   app$set_inputs(ImageRES = "4k")
   app$set_inputs(sw = "13.2")
   app$set_inputs(flen = "8.8")
   app$set_inputs(drone = "P4P")
-  app$set_inputs(alt = 10)
   app$set_inputs(alt = 14.7)
-  app$set_inputs(takeof = 0)
-  app$set_inputs(takeof = 1.2)
-  app$set_inputs(takeof = character(0))
   app$set_inputs(takeof = 1.8)
   app$set_inputs(Date = "2019-08-13")
   app$set_inputs(obs = "Observer 1")
-  app$set_inputs(plot_click = c(1851.80384720203, 1002.98662904251, 506.8125, 333.046875,
-      506.8125, 333.046875, 1, 1, 0.999999999999773, 4096, 2294.71511627907, -133.71511627907,
-      59.04, 1049.76, 645.56, 58.04, character(0), character(0)), allow_no_input_binding_ = TRUE,
-      priority_ = "event")
-  app$set_inputs(plot_brush = c(1851.803847202, 2587.5414925509, 1002.9866290425,
-      1457.6559604379, 506.8125, 684.8125, 333.046875, 443.046875, 506.8125, 684.8125,
-      333.046875, 443.046875, 1, 1, 0.999999999999773, 4096, 2294.71511627907, -133.71511627907,
-      59.04, 1049.76, 645.56, 58.04, character(0), character(0), "xy", "plot_brush",
-      "imagePlot"), allow_no_input_binding_ = TRUE)
-  app$click("crop")
-  app$set_inputs(plot_click = c(2054.3818102982, 1206.52210019011, 340.8125, 321.046875,
-      340.8125, 321.046875, 1, 1, 1836.32401791562, 2603.02132183728, 1457.6559604379,
-      1002.9866290425, 59.0400000000002, 1049.76, 645.56, 58.04, character(0), character(0)),
-      allow_no_input_binding_ = TRUE, priority_ = "event")
-  app$set_inputs(plot_click = c(2370.124401545, 1203.42658458965, 748.8125, 317.046875,
-      748.8125, 317.046875, 1, 1, 1836.32401791562, 2603.02132183728, 1457.6559604379,
-      1002.9866290425, 59.0400000000002, 1049.76, 645.56, 58.04, character(0), character(0)),
-      allow_no_input_binding_ = TRUE, priority_ = "event")
+
+  # 4. Simulação de medição (Encurtada para eficiência)
+  app$set_inputs(plot_brush = c(1851.8, 2587.5, 1002.9, 1457.6, 506.8, 684.8, 333.0, 443.0, 506.8, 684.8, 333.0, 443.0, 1, 1, 0.9, 4096, 2294.7, -133.7, 59.0, 1049.7, 645.5, 58.0, character(0), character(0), "xy", "plot_brush", "imagePlot"), allow_no_input_binding_ = TRUE)
+  app$click("crop", wait_ = FALSE)
+  app$wait_for_idle()
+
+  app$set_inputs(plot_click = c(2054.3, 1206.5, 340.8, 321.0, 340.8, 321.0, 1, 1, 1836.3, 2603.0, 1457.6, 1002.9, 59.0, 1049.7, 645.5, 58.0, character(0), character(0)), allow_no_input_binding_ = TRUE, priority_ = "event")
+  app$set_inputs(plot_click = c(2370.1, 1203.4, 748.8, 317.0, 748.8, 317.0, 1, 1, 1836.3, 2603.0, 1457.6, 1002.9, 59.0, 1049.7, 645.5, 58.0, character(0), character(0)), allow_no_input_binding_ = TRUE, priority_ = "event")
+
+  # 5. Salvar e Limpar
   app$click("saveBtn")
+  app$wait_for_idle()
+
   app$click("clearBtn")
+  app$wait_for_idle()
+
   app$click("confirm_reset")
-  app$click("confirm_reset")
-  app$set_inputs(mTable_rows_current = 1, allow_no_input_binding_ = TRUE)
-  app$set_inputs(mTable_rows_all = 1, allow_no_input_binding_ = TRUE)
-  app$set_inputs(mTable_state = c(1782743336559, 0, 10, "", TRUE, FALSE, TRUE, c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE,
-      TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE,
-      TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE,
-      TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE)), allow_no_input_binding_ = TRUE)
-  app$set_inputs(mTable_state = c(1782743341653, 0, 100, "", TRUE, FALSE, TRUE, c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE,
-      TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE,
-      TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE,
-      TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE,
-      "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE)), allow_no_input_binding_ = TRUE)
+  app$wait_for_idle()
+
   app$expect_values()
 })
